c878dd59dd837e6356a7c176909331f21d95abdf
Gfhjkmfl@1@1 <kostechkina@gmail.com>
2025-04-13 12:40:14 +0400

    change router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import App from "./app/App";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
